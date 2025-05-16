da69b420f0eb104ae82c313ab7e208c6255f7991
UwaisN7 <uwaisn73@gmail.com>
2025-05-16 09:18:02 +0200

    Mneu again

---
+++ Assets/Main Menu/Scripts/MainMenuController.cs
@@ -0,0 +1,37 @@
+//Make Your MAIN MENU Quickly! | Unity UI Tutorial For Beginners
+//ReHope Games 
+//Version 2 
+// 14 April 2025
+// https://youtu.be/DX7HyN7oJjE?si=p1XLep7gywfDdfCI
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+        Time.timeScale = 1;
+    }
+    public void howToPlay()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+    public void backToMainMenu()
+    {
+        SceneManager.LoadSceneAsync("Main menu"); //Dont know why the number 0 doesnt work but making a string works too 
+    }
+
+    public void Story()
+    {
+        SceneManager.LoadSceneAsync(3);
+    }
+    public void quit()
+    {
+        Application.Quit();
+        Debug.Log("RageQuit");
+    }
+
+
+
+}
