e72bbbc4e6cedc90dee147f8da5e28662cc71e93
Skyxiao68 <ben.lai.3368@gmail.com>
2025-06-15 03:19:00 +0200

    All juice sounds added (should/shouldnt be final)

--- Assets/Destruction.cs
+++ Assets/Destruction.cs
@@ -5,34 +5,56 @@ using UnityEngine;
 public class Destruction : MonoBehaviour
 {
     [Header("Effects")]
-    public ParticleSystem destructionParticles; 
-   
+    public ParticleSystem destructionParticles;
     public CinemachineImpulseSource destruction;
-  
 
-    private void Start()
-    {
-        destruction = GetComponent<CinemachineImpulseSource>();
-
-    }
+    [Header("Sound")]
+    public AudioClip destructionSound;
+    [Range(0f, 1f)] public float volume = 0.8f;
+    public float minPitch = 0.9f;
+    public float maxPitch = 1.1f;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Bullet"))
         {
 
-            CameraShakeManager.instance.CameraShake(destruction);
+            PlayDestructionSound();
+
+
+            if (destruction != null)
+            {
+                CameraShakeManager.instance.CameraShake(destruction);
+            }
+
 
             if (destructionParticles != null)
             {
                 Instantiate(destructionParticles, transform.position, Quaternion.identity);
             }
 
-           
-          
 
-          
             Destroy(gameObject);
+        }
+    }
+
+    void PlayDestructionSound()
+    {
+        if (destructionSound != null)
+        {
+
+            GameObject tempGO = new GameObject("TempAudio");
+            tempGO.transform.position = transform.position;
+            AudioSource audioSource = tempGO.AddComponent<AudioSource>();
+
+
+            audioSource.clip = destructionSound;
+            audioSource.volume = volume;
+            audioSource.pitch = Random.Range(minPitch, maxPitch);
+            audioSource.Play();
+
+
+            Destroy(tempGO, destructionSound.length);
         }
     }
 }
--- Assets/Player assets/Scripts/Heal.cs
+++ Assets/Player assets/Scripts/Heal.cs
@@ -1,16 +1,33 @@
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Heal : MonoBehaviour
 {
     public int heal = 10;
+    [Header("Healing Sound")]
+    public AudioClip HealSound;
+    [Range(0f, 1f)] public float volume = 0.7f;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.TryGetComponent<Health>(out Health amountHealed)) 
-        { 
-        
-         amountHealed.Heal (heal);
-         Destroy(gameObject);
-        
+        if (collision.TryGetComponent<Health>(out Health amountHealed))
+        {
+
+            PlayHealSound();
+
+            amountHealed.Heal(heal);
+
+            Destroy(gameObject);
+
+        }
+    }
+
+    void PlayHealSound()
+    {
+        if (HealSound != null)
+        {
+            // Play at pickup position with specified volume
+            AudioSource.PlayClipAtPoint(HealSound, transform.position, volume);
         }
     }
 }
