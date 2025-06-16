8a2199b7795338f46a5aaae5a6c4f4e97761474f
Skyxiao68 <ben.lai.3368@gmail.com>
2025-06-16 12:42:00 +0200

    sound touch up

--- Assets/Story/Scripts/Story note.cs
+++ Assets/Story/Scripts/Story note.cs
@@ -11,24 +11,46 @@ public class Item : MonoBehaviour
     public GameObject storyTime;
     public GameObject playerUi;
     public GameObject resumeButton;
-    
+
     public TextMeshProUGUI interact;
 
+
+    [Header("Pickup Sound")]
+    public AudioClip pickupSound;
+    [Range(0f, 1f)] public float volume = 0.7f;
+    public float minPitch = 0.95f;
+    public float maxPitch = 1.05f;
+
     private bool playerInRange;
-    private void Start ()
+
+    private void Start()
     {
         storyTime.SetActive(false);
         interact.gameObject.SetActive(false);
-       
     }
 
     private void Update()
     {
         if (playerInRange && Input.GetKeyDown(KeyCode.E))
         {
+            PlayPickupSound(); 
             StoryTime();
-            
-        
+        }
+    }
+
+    void PlayPickupSound()
+    {
+        if (pickupSound != null)
+        {
+         
+            GameObject tempGO = new GameObject("TempAudio");
+            tempGO.transform.position = transform.position;
+            AudioSource audioSource = tempGO.AddComponent<AudioSource>();
+            audioSource.clip = pickupSound;
+            audioSource.volume = volume;
+            audioSource.pitch = Random.Range(minPitch, maxPitch);
+            audioSource.Play();
+            Destroy(tempGO, pickupSound.length); 
         }
     }
 
@@ -39,10 +61,9 @@ public class Item : MonoBehaviour
             playerInRange = true;
             interact.gameObject.SetActive(true);
             interact.SetText("Press E to pickup");
-
         }
-          
     }
+
     public void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
@@ -51,39 +72,32 @@ public class Item : MonoBehaviour
             interact.gameObject.SetActive(false);
         }
     }
+
     public void Resume()
     {
         NoMoreStoryTime();
-         Destroy(gameObject);
-
+        Destroy(gameObject);
     }
+
     void StoryTime()
     {
         storyTime.SetActive(true);
         Pause.SetActive(false);
         playerUi.SetActive(false);
-
-        AudioListener.volume = 0;
-
         Time.timeScale = 0f;
     }
+
     void NoMoreStoryTime()
     {
         storyTime.SetActive(false);
         Pause.SetActive(true);
         playerUi.SetActive(true);
         resumeButton.SetActive(false);
-
-
-        AudioListener.volume = 1;
-
-        Time.timeScale = 1f; 
-       
-    
+        Time.timeScale = 1f;
     }
+
     public void Bossssss()
     {
         //Scene
     }
-
 }
